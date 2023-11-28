e2465f0b8dc54f1352a548bf41dcc711aecdc0ac
Debaucus <60629996+Debaucus@users.noreply.github.com>
2023-11-28 21:29:21 +0000

    Updated to match Kick.com changes.

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
-console.log("Launching KickBot Chat Moderator...");
+console.log("Launching KickBot Chat Connection...");
 
 // WS - websocker opened to read chat messages.
-import ws from "ws";
-
-// Pusher is the service used for chatting.
-const chat = new ws(
-  "wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.4.0&flash=false"
-);
-//Kick.com channels to join and monitor.
-const chatroomList = [15751, 19535];
+import WebSocket from "ws";
+//Kick.com channels to join and monitor. Array of values.
+//To get a channel ID, load a pop-up chat of the channel you wish to monitor.
+//Check that request labeled `101` and use that channel number listed there.
+const chatroomList = [4598];
 
 chatroomList.forEach((chatroomNumber) => {
   // Open websocket to each streamer.
+
+  // Pusher is the service used for chatting.
+  const chat = new WebSocket(
+    "wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.6.0&flash=false"
+  );
+
   chat.on("open", function open() {
     chat.send(
       JSON.stringify({
         event: "pusher:subscribe",
-        data: { auth: "", channel: `chatrooms.${chatroomNumber}` },
+        data: { auth: "", channel: `chatrooms.${chatroomNumber}.v2` },
       })
     );
+
     console.log(
       "\x1b[33m%s\x1b[0m",
       "Connected to Kick.com Streamer Chat: " + chatroomNumber
     );
   });
-});
 
-chat.on("message", function message(data) {
-  try {
-    // Order to get things.
-    // decode message to get components needed.
-    let dataString = data.toString();
-    //console.log(JSON.parse(dataString));
+  chat.on("error", console.error);
 
-    let jsonData = JSON.parse(dataString);
-    //console.log(jsonData.data);
+  chat.on("close", function close() {
+    console.log("Connection closed for chatroom: " + chatroomNumber);
+  });
 
-    let jsonDataSub = JSON.parse(jsonData.data);
-    //console.log(jsonMessageFull);
+  chat.on("message", function message(data) {
+    try {
+      // Order to get things.
+      // decode message to get components needed.
+      const dataString = data.toString();
+      //console.log(JSON.parse(dataString));
 
-    // Values - id (of message), message, created_at, type, chatroom_id, role
-    let jsonMessage = jsonDataSub.message;
-    // Values - username, name, role, id (of user)
-    let jsonUser = jsonDataSub.user;
+      const jsonData = JSON.parse(dataString);
+      //console.log(jsonData.data);
 
-    // Log the message in console for tracker purposes.
-    console.log(
-      new Date().toLocaleTimeString() +
-        " - " +
-        jsonMessage.chatroom_id +
-        " - " +
-        jsonMessage.id +
-        " - " +
-        jsonUser.username +
-        " - " +
-        jsonMessage.message
-    );
-  } catch (error) {
-    //console.log(error);
-  }
+      const jsonDataSub = JSON.parse(jsonData.data);
+
+      // Log the message in console for tracker purposes.
+      console.log(
+        new Date().toLocaleTimeString() +
+          " - " +
+          jsonDataSub.chatroom_id +
+          " - " +
+          jsonDataSub.sender.username +
+          " - " +
+          jsonDataSub.content
+      );
+    } catch (error) {
+      //console.log(error);
+    }
+  });
 });
